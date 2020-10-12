78f313c7979a6117eb9ef8c43c0645a83f50d2d1
alcamelo <anderson.camelo@castgroup.com.br>
2020-10-12 20:06:40 -0300

    Remove teste vazio.

--- src/test/java/br/com/dbccompany/votacaocooperado/VotacaoCooperadoApplicationTests.java
+++ src/test/java/br/com/dbccompany/votacaocooperado/VotacaoCooperadoApplicationTests.java
@@ -1,13 +1,9 @@
 package br.com.dbccompany.votacaocooperado;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class VotacaoCooperadoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
